11cc144c507e8f6632d14f4b9eacfd79d053409b
Ivan Diaz <ivanid22@gmail.com>
2020-12-02 19:39:43 -0300

    Add picture preview to SubmitPostingForm

--- src/components/SubmitPostingForm/SubmitPostingForm.js
+++ src/components/SubmitPostingForm/SubmitPostingForm.js
@@ -2,12 +2,12 @@ import React, { useState, useRef } from 'react';
 import {
   FormControl,
   TextField,
-  FormHelperText,
   Button,
 } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
 import PropTypes from 'prop-types';
 import styles from './SubmitPostingForm.module.scss';
+import defaultPicture from '../../assets/default-posting-picture.png';
 
 const buttonStyles = makeStyles({
   root: {
@@ -50,10 +50,12 @@ const formFieldStyles = makeStyles({
 
 const SubmitPostingForm = ({ submitPosting, submitStatus }) => {
   const [formData, setFormData] = useState({});
+  const [picture, setPicture] = useState(null);
   const textFieldClasses = textfieldStyles();
   const buttonClasses = buttonStyles();
   const formFieldClasses = formFieldStyles();
   const fileInputRef = useRef(null);
+  const fileReader = new FileReader();
 
   const onSubmit = event => {
     event.preventDefault();
@@ -67,11 +69,29 @@ const SubmitPostingForm = ({ submitPosting, submitStatus }) => {
     submitPosting(data);
   };
 
+  const onFileSelect = () => {
+    if (fileInputRef.current.files.length > 0) {
+      fileReader.readAsDataURL(fileInputRef.current.files[0]);
+    }
+  };
+
+  fileReader.addEventListener('load', () => {
+    setPicture(fileReader.result);
+  });
+
   return (
     <form className={styles.form} onSubmit={onSubmit}>
       <FormControl className={formFieldClasses.root}>
-        <input ref={fileInputRef} type="file" id="picture-input" value={formData.picture} aria-describedby="my-helper-text" />
-        <FormHelperText id="my-helper-text">Select a picture for your posting</FormHelperText>
+        <input ref={fileInputRef} type="file" id="picture-input" className={styles.fileInput} onChange={onFileSelect} />
+        <div className={styles.pictureContainer}>
+          <img src={picture || defaultPicture} alt="Posting description" />
+        </div>
+        <Button
+          className={buttonClasses.root}
+          onClick={() => fileInputRef.current.click()}
+        >
+          Add picture
+        </Button>
       </FormControl>
       <FormControl className={formFieldClasses.root}>
         <TextField type="text" required className={textFieldClasses.root} variant="outlined" label="Title" placeholder="Title for your posting" onChange={e => setFormData({ ...formData, title: e.target.value })} />
