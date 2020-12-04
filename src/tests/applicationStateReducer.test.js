f9c00b1e40ad84a8f15e8054c5de73e99df8a1d9
Ivan Diaz <ivanid22@gmail.com>
2020-12-04 13:28:18 -0300

    Add reducer test cases

--- src/tests/applicationStateReducer.test.js
+++ src/tests/applicationStateReducer.test.js
@@ -16,4 +16,28 @@ describe('applicationStateReducer', () => {
       userProfile,
     });
   });
+
+  it('should clear the current user profile from state', () => {
+    const state = {
+      userProfile: {
+        email: 'test@test.com',
+        password: 'test',
+      },
+    };
+
+    const action = {
+      type: ApplicationStateActions.CLEAR_USER_PROFILE,
+    };
+
+    expect(applicationStateReducer(state, action)).toEqual({
+      userProfile: {},
+    });
+  });
+
+  it('should set the user profile update status', () => {
+    const action = { type: ApplicationStateActions.SET_UPDATING_USER_PROFILE_STATUS, status: 'test' };
+    expect(applicationStateReducer(null, action)).toEqual({
+      updateUserProfileStatus: 'test',
+    });
+  });
 });
